3b4c51d99925dd9aa30da4a549ad6d164535628d
Vinicius-Braz17 <godinhobraz@gmail.com>
2024-01-30 17:33:25 -0300

    instalacao do bootstrap

--- src/Pages/Home/Pag3Home.js
+++ src/Pages/Home/Pag3Home.js
@@ -8,11 +8,11 @@ export default function Pag3Home({ lang }) {
 
             <div className={s.painel_esquerda}>
                 <img alt='Foto profissional do Vinícius' className={s.foto_profissional} src={FOTO} />
-                <h1 className={s.botoes_introducao}>Sobre mim</h1>
-                <h1 className={s.botoes_introducao}>Conhecimentos</h1>
+                {lang === 'pt' ? (<h1 className={s.botoes_introducao}>Sobre mim</h1>) : (<h1 className={s.botoes_introducao}>About Me</h1>)}
+                {lang === 'pt' ? (<h1 className={s.botoes_introducao}>Meus conhecimentos</h1>) : (<h1 className={s.botoes_introducao}>My Knowlodges</h1>)}
                 <h1 className={s.botoes_introducao}>Hard-Skills</h1>
                 <h1 className={s.botoes_introducao}>Soft-Skills</h1>
-                <h1 className={s.botoes_introducao}>Por quê me contratar?</h1>
+                {lang === 'pt' ? (<h1 className={s.botoes_introducao}>Por quê me contratar?</h1>) : (<h1 className={s.botoes_introducao}>Why should you hire me?</h1>)}
             </div>
             <div className={s.painel_direita}>
                 <article className={s.cardzin}>
